32987f6e5ed3582d5968249c1f80e8e538c309af
wendy <2313106773@qq.com>
2019-10-14 17:57:16 +0800

    scdata

--- src/views/SC/SCdata.vue
+++ src/views/SC/SCdata.vue
@@ -1,5 +1,169 @@
 <template>
-  <div>
+  <div class='scdata'>
+    <div class="title">
+      <div class="selects">
+        <Select v-model="select.mClTypeValue" style="width:200px" placeholder="材料类型">
+          <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
+        <Select v-model="select.mItemBid" style="width:200px" placeholder="工程标段">
+          <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
+        <DatePicker type="date" placeholder="开始时间" style="width: 200px" v-model="select.start_time"></DatePicker>
+        <DatePicker type="date" placeholder="结束时间" style="width: 200px" v-model="select.end_time"></DatePicker>
+      </div>
+      <div class="btn">
+        <span>
+          <img src="@/assets/img/sc/search.png" alt />
+        </span>
+        <span>搜索</span>
+      </div>
+    </div>
+    <div class="content">
+<!-- 产量列表 -->
+<Table :loading="loading" height="300" border :columns="listTitle" :data="datalist"></Table>
+<Switch v-model="loading"></Switch>
+<div style="margin: 10px;overflow: hidden">
+        <div style="float: right;">
+            <Page :total="page.totaldata" :current.sync="page.current" @on-change="changePage"></Page>
+        </div>
+    </div>
+    </div>
 
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      loading: true,
+      select: {
+        start_time: '',
+        end_time: '',
+        mClTypeValue: '',
+        mItemBid: ''
+      },
+      page: {
+        // 数据总条数
+        totaldata: 1,
+        // 当前页数
+        current: 1,
+        // 每页条数
+        rows: 10
+      },
+      cityList: [
+        {
+          value: 'New York',
+          label: 'New York'
+        },
+        {
+          value: 'London',
+          label: 'London'
+        },
+        {
+          value: 'Sydney',
+          label: 'Sydney'
+        },
+        {
+          value: 'Ottawa',
+          label: 'Ottawa'
+        },
+        {
+          value: 'Paris',
+          label: 'Paris'
+        },
+        {
+          value: 'Canberra',
+          label: 'Canberra'
+        }
+      ],
+      listTitle: [
+        {
+          title: '生产日期',
+          key: 'mBhDateTime',
+          width: 180
+        },
+        {
+          title: '材料类型',
+          key: 'mClTypeName'
+        },
+        {
+          title: '生产总量',
+          key: 'mBhItemNO'
+        },
+        {
+          title: '油石比',
+          key: 'mBnYSB'
+        },
+        {
+          title: '出料温度',
+          key: 'mBhItemTemp'
+        },
+        {
+          title: '合格',
+          key: 'key1'
+        },
+        {
+          title: '温度预警',
+          key: 'mBhItemTempAlarm'
+        },
+        {
+          title: '油石比预警',
+          key: 'mBhYSBAlarm'
+        },
+        {
+          title: '级配预警',
+          key: 'key2'
+        }
+      ],
+      datalist: []
+    }
+  },
+  mounted () {
+    // 得到总览数据
+    this.getData()
+  },
+  methods: {
+    getData () {
+      let obj = {
+        mUserID: this.comFun.getCookie('roadmUserID'),
+        mItemID: this.$store.state.mItemID,
+        page: this.page.current,
+        rows: this.page.rows,
+        start_time: this.select.start_time,
+        end_time: this.select.end_time,
+        // 材料类型ID
+        mClTypeValue: this.select.mClTypeValue,
+        // 项目标段ID
+        mItemBid: this.select.mItemBid
+
+      }
+      // console.log(JSON.stringify(obj))
+      this.comFun.post('/Produce_J_G/productData', obj, this).then((rs) => {
+        console.log(JSON.stringify(rs))
+        if (rs.code === 0) {
+          // 总页数
+          this.page.totaldata = rs.data.total
+
+          // 列表 图
+          // 处理列表数据
+          rs.data.data_list.map((item, index, arr) => {
+            arr[index].mBhItemTemp = item.mBhItemTemp + '℃'
+            // 预警：0:无预警 1:产生预警
+            arr[index].mBhItemTempAlarm = item.mBhItemTempAlarm ? '是' : '否'
+            arr[index].mBhYSBAlarm = item.mBhYSBAlarm ? '是' : '否'
+            arr[index].mBnYSB = item.mBnYSB + '%'
+          })
+          this.datalist = rs.data.data_list
+          // 各个标段总量统计柱状图
+        // this.dataBar1 = this.handleBarData(rs.data.BhBidData, 'name', 'value')
+        }
+      }, (err) => { console.log(err) })
+    },
+    changePage () {
+      // 更换页数
+      this.getData()
+      console.log('切换page:' + this.page.current)
+    }
+  }
+}
+</script>
--- src/views/SC/SChome.vue
+++ src/views/SC/SChome.vue
@@ -1,82 +1,13 @@
 <style lang="less">
-.schome {
-  background: #fff;
-  height: 100%;
-  padding:40px;
-  .title{
+.content{
+  margin-top: 30px;
+  .bar1{
+    margin-bottom: 25px;
+  }
+  .bottom{
     display: flex;
-    .selects{
-      width: 900px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      // 选择器样式修改
-      .ivu-select-single .ivu-select-selection{
-        height: 58px;
-        line-height: 58px;
-        background: #00C4C0FF;
-        .ivu-select-placeholder{
-          line-height: 58px;
-          height: 58px;
-          color:#fff;
-          font-size: 25px;
-        }
-        .ivu-select-selected-value{
-          height: 58px;
-          color:#fff;
-          font-size: 25px;
-          line-height: 58px;
-        }
-        .ivu-select{
-          color:#fff;
-          font-size: 25px;
-        }
-        // 箭头
-        .ivu-select-arrow{
-          font-size: 28px;
-          color:#fff;
-        }
-      }
-      // 选择日期样式修改
-      .ivu-input{
-        background: #00C4C0FF;
-        color:#fff;
-         height: 58px;
-         font-size: 27px;
-
-      }
-      input::-webkit-input-placeholder{color:#fff!important;}
-      .ivu-input-suffix{
-        margin-right: 10px;
-      }
-      .ivu-input-suffix i{
-        font-size: 28px;
-        color:#fff;
-        line-height: 58px;
-      }
-    }
-    .btn{
-      width:154px;
-      box-sizing: border-box;
-      height:60px;
-      background:rgba(28,106,171,1);
-      border-radius:30px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      padding:18px 22px;
-      margin-left: 60px;
-      img{
-        margin-top: 5px;
-        width: 40px;
-        height: 40px;
-      }
-      span{
-        font-size: 27px;
-        color:#fff;
-        margin-top: 4px;
-      }
-    }
+    justify-content: space-between;
+    margin-top: 30px;
   }
 }
 </style>
@@ -85,16 +16,16 @@
   <div class="schome">
     <div class="title">
       <div class="selects">
-        <Select v-model="bd" style="width:200px" placeholder="工程标段">
-          <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        <Select v-model="select.mClTypeValue" style="width:200px" placeholder="材料类型">
+          <Option v-for="item in mClTypeValueList" :value="item.value" :key="item.value">{{ item.label }}</Option>
         </Select>
-        <Select v-model="material" style="width:200px" placeholder="材料类型">
-          <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        <Select v-model="select.mItemBid" style="width:200px" placeholder="工程标段">
+          <Option v-for="item in mItemBidList" :value="item.value" :key="item.value">{{ item.label }}</Option>
         </Select>
-        <DatePicker type="date" placeholder="开始时间" style="width: 200px" v-model="time.begin"></DatePicker>
-        <DatePicker type="date" placeholder="结束时间" style="width: 200px" v-model="time.end"></DatePicker>
+        <DatePicker type="date" placeholder="开始时间" style="width: 200px" v-model="select.start_time" @on-change="changeType0"></DatePicker>
+        <DatePicker type="date" placeholder="结束时间" style="width: 200px" v-model="select.end_time" @on-change="changeType1"></DatePicker>
       </div>
-      <div class="btn">
+      <div class="btn" @click="getData()">
         <span>
           <img src="@/assets/img/sc/search.png" alt />
         </span>
@@ -103,41 +34,47 @@
     </div>
     <div class="content">
       <div class="top">
-<BarChart :id="'bar1'" :data="dataBar1" style="height:180px"></BarChart>
+        <div class="bar1">
+<BarChart :id="'bar1'" :data="dataBar1" style="height:200px"></BarChart>
+        </div>
+        <div class="bar2">
+<BarChart :id="'bar2'" :data="dataBar2" style="height:200px"></BarChart>
+        </div>
+
       </div>
       <div class="bottom">
-        <div class="left">
- <PieChart :id="'pie1'" :data="dataPie1" style="height:180px"></PieChart>
- <PieChart :id="'pie2'" :data="dataPie2" style="height:180px"></PieChart>
-        </div>
-        <div class="right">
 
-        </div>
+ <PieChart :id="'pie1'" :data="dataPie1" style="height:260px;width:350px"></PieChart>
+ <PieChart :id="'pie2'" :data="dataPie2" style="height:260px;width:350px"></PieChart>
+ <PieChart :id="'pie3'" :data="dataPie3" style="height:260px;width:350px"></PieChart>
+
       </div>
     </div>
   </div>
 </template>
 <script>
+import '@/assets/css/selects.css'
 import PieChart from '@/components/PieChart.vue'
 import BarChart from '@/components/BarChart.vue'
 export default {
   data () {
     return {
-      time: {
-        begin: '',
-        end: ''
+      select: {
+        start_time: '',
+        end_time: '',
+        mClTypeValue: '',
+        mItemBid: ''
       },
-      material: '',
-      bd: '',
       // 各个标段总量统计柱状图
       dataBar1: null,
+      dataBar2: null,
       // 预警类型饼图
       dataPie1: null,
       // 预警级别分类饼图
       dataPie2: null,
       // 质量，按合格率统计饼图
       dataPie3: null,
-      cityList: [
+      mClTypeValueList: [
         {
           value: 'New York',
           label: 'New York'
@@ -145,22 +82,12 @@ export default {
         {
           value: 'London',
           label: 'London'
-        },
+        }
+      ],
+      mItemBidList: [
         {
-          value: 'Sydney',
-          label: 'Sydney'
-        },
-        {
-          value: 'Ottawa',
-          label: 'Ottawa'
-        },
-        {
-          value: 'Paris',
-          label: 'Paris'
-        },
-        {
-          value: 'Canberra',
-          label: 'Canberra'
+          value: '标段1',
+          label: '标段1'
         }
       ]
     }
@@ -170,17 +97,39 @@ export default {
     this.getData()
   },
   methods: {
+    changeType0 (val) {
+      console.log(val)
+      this.select.start_time = val
+    },
+    changeType1 (val) {
+      console.log(val)
+      this.select.end_time = val
+    },
     getData () {
       let obj = {
         mUserID: this.comFun.getCookie('roadmUserID'),
-        mItemID: this.$store.state.mItemID
+        mItemID: this.$store.state.mItemID,
+        mClTypeValue: this.select.mClTypeValue,
+        mItemBid: this.select.mItemBid,
+        start_time: this.select.start_time,
+        end_time: this.select.end_time
       }
+      console.log(typeof this.select.start_time)
+
       console.log(JSON.stringify(obj))
       this.comFun.post('/Produce_J_G/index', obj, this).then((rs) => {
         console.log(JSON.stringify(rs))
         if (rs.code === 0) {
           // 各个标段总量统计柱状图
-        // this.dataBar1 = this.handleBarData(rs.data.BhBidData, 'name', 'value')
+          this.dataBar1 = this.handleBarData(rs.data.BhBidData, 'name', 'value')
+          // 各个材料总量统计柱状图
+          this.dataBar2 = this.handleBarData2(rs.data.BhClTypeData, 'name', 'value')
+          // 预警类型饼图
+          this.dataPie1 = this.handlePieData(rs.data.AlarmData.data, 'type_name', 'rep', '预警类型分类统计')
+          // 预警级别分类饼图
+          this.dataPie2 = this.handlePieData(rs.data.AlarmLevelData.data, 'level_name', 'rep', '预警级别分类统计')
+          // 质量按合格率统计饼图
+          this.dataPie3 = this.handlePieData(rs.data.BhStandardData, 'key', 'rep', '质量统计', 3)
         }
       }, (err) => { console.log(err) })
     },
@@ -193,9 +142,13 @@ export default {
         seriesdata.push(data[i][val])
       }
       let option = {
-        color: ['#6996F3'],
+        title: {
+          text: '拌合站产量按标段统计',
+          textVerticalAlign: 'top'
+        },
+        color: ['#333'],
         textStyle: {
-          color: '#9FC9F7'
+          color: '#333'
         },
         tooltip: {
           trigger: 'axis',
@@ -204,8 +157,8 @@ export default {
           }
         },
         grid: {
-          top: '3%',
-          left: '3%',
+          top: '22%',
+          left: '0%',
           right: '4%',
           bottom: '3%',
           containLabel: true
@@ -222,7 +175,7 @@ export default {
         yAxis: [
           {
             type: 'value',
-            splitLine: { show: true, lineStyle: { color: ['#333'] } } // 网格线
+            splitLine: { show: true, lineStyle: { color: ['#ddd'] } } // 网格线
           }
         ],
         series: [
@@ -239,7 +192,79 @@ export default {
                 }
               }
             },
-            barWidth: '60%',
+            barWidth: '40px',
+            data: seriesdata
+          }
+        ]
+      }
+      return option
+    },
+    handleBarData2 (data, name, val) {
+      let xAxisdata = []
+      let seriesdata = []
+      for (var i = 0; i < data.length; i++) {
+        xAxisdata.push(data[i][name])
+        seriesdata.push(data[i][val])
+      }
+      let option = {
+        title: {
+          text: '拌合站产量按材料类型统计',
+          textVerticalAlign: 'top'
+        },
+        color: ['#333'],
+        textStyle: {
+          color: '#333'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: { // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        grid: {
+          top: '22%',
+          left: '0%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        dataZoom: [
+          {
+            show: true,
+            start: 0,
+            end: 40
+          }
+        ],
+        xAxis: [
+          {
+            type: 'category',
+            data: xAxisdata,
+            axisTick: {
+              alignWithLabel: true
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value',
+            splitLine: { show: true, lineStyle: { color: ['#ddd'] } } // 网格线
+          }
+        ],
+        series: [
+          {
+            name: '.',
+            type: 'bar',
+            itemStyle: {
+              normal: {
+                // 这里是重点
+                color: function (params) {
+                // 注意，如果颜色太少的话，后面颜色不会自动循环，最好多定义几个颜色
+                  var colorList = ['#14C6CA', '#6996F3']
+                  return colorList[params.dataIndex]
+                }
+              }
+            },
+            barWidth: '25px',
             data: seriesdata
           }
         ]
@@ -247,27 +272,39 @@ export default {
       return option
     },
     // 处理饼图数据
-    handlePieData (data, name, rep, title) {
+    handlePieData (data, name, rep, title, type, val) {
       let legenddata = []
       let seriesdata = []
-      for (var i = 0; i < data.length; i++) {
+      for (let i = 0; i < data.length; i++) {
         legenddata.push(data[i][name])
         seriesdata.push({
           value: data[i][rep],
           name: data[i][name]
         })
       }
+      if (type === 3) {
+        seriesdata = [
+          { value: data[1][rep], name: '不合格：' + data[1].value },
+          { value: data[2][rep], name: '合格：' + data[2].value }
+        ]
+      }
       // 给饼图赋值
       let option = {
+        title: {
+          text: title,
+          textVerticalAlign: 'top'
+        },
         tooltip: {
           trigger: 'item',
           formatter: '{a} <br/>{b} : {c} ({d}%)'
         },
+
         legend: {
           orient: 'vertical',
           left: 'left',
+          top: '29',
           textStyle: {
-            color: '#9FC9F7'
+            color: '#333'
           },
           data: legenddata
         },
@@ -277,7 +314,7 @@ export default {
             name: title,
             type: 'pie',
             radius: ['24%', '55%'],
-            center: ['55%', '50%'],
+            center: ['55%', '70%'],
             data: seriesdata,
             itemStyle: {
               emphasis: {
